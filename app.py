3e11cc5c65be63bb3d40add6c1aadd0489cca7b6
Stephan <stephan.semerad@gmail.com>
2022-02-01 12:50:20 +0100

    created api

--- app.py
+++ app.py
@@ -1,21 +1,69 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask import request
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db  = SQLAlchemy(app)
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    email = db.column(db.String, unique=True, nullable=False)
+    id      = db.Column(db.Integer, primary_key=True)
+    name    = db.Column(db.String, nullable=False)
+    email   = db.Column(db.String, unique=True, nullable=False)
 
-
+    def as_dict(self):  
+        dictionary =  {c.name: str(getattr(self, c.name)) for c in self.__table__.columns}
+        return dictionary
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return '<p>Welcome to my User API</p>'
+
+@app.route("/user_create")
+def user_create():
+    print('user_create')
+    
+    name  = request.args.get('name', None)
+    email = request.args.get('email', None)
+
+    print('name: ', name)
+    print('email: ', email)    
+    
+    if name == None:
+        return {'error': 'name is required'}
+    
+    if email == None:
+        return {'error': 'email is required'}
+    
+    email   = email.lower().replace(' ', '')
+    user    = db.session.query(User).filter(User.email==email).first()
+    if user != None:
+        return {'error': f'User with email: {email} already exists'}
+    
+    query = User()
+    query.name = name
+    query.email = email 
+    db.session.add(query)
+    db.session.commit()
+    
+    return {'success': f'User {name} created'}
+
+
+    
+
+@app.route("/user_update")
+def user_update():
+    pass
+
+@app.route("/user_review")
+def user_review():
+    pass
+
+@app.route("/user_delete")
+def user_delete():
+    pass
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    db.create_all()
+    app.run(host='0.0.0.0', port=5000, debug=True)
